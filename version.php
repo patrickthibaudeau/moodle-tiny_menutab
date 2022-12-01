670988237a4ed07156142bbe22a07125f3fcb52c
Patrick Thibaudeau <thibaud@yorku.ca>
2022-12-01 15:13:00 -0500

    Default files

---
+++ version.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     tiny_menutab
+ * @copyright   2022 Patrick Thibaudeau ,thibaud@yorku.ca>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'tiny_menutab';
+$plugin->release = '0.1.0';
+$plugin->version = 2022120100;
+$plugin->requires = 2021051700;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->dependencies = [
+    'format_menutab' => 2022111900,
+];
