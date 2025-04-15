c3e15f9170361d1a525df0ac3d5d5312c261ecf8
Patrick Thibaudeau <thibaud@yorku.ca>
2025-04-15 09:58:01 -0400

    Updated version.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_menutab';
-$plugin->release = '0.1.0';
-$plugin->version = 2025041403;
+$plugin->release = '1.0.0';
+$plugin->version = 2025041500;
 $plugin->requires = 2022041900;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
